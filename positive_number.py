34a4c8df5f49bc2a907562378e5a2013d0dc59db
Shubham Kumar <105226955+shubhamraj1265@users.noreply.github.com>
2024-07-05 01:20:22 +0530

    Create positive_number.py
    Write a program to check if a number is positive or Not.

---
+++ positive_number.py
@@ -0,0 +1,8 @@
+# Write a program to check if a number is positive or Not.
+number = int(input("Enter any number: "))
+print(number)
+
+if number > 0:
+    print("Number is positive")
+else:
+    print("Number is negative")
